d8c65c2e548a6a895e53e1791a19233844d81504
unknown <grall.emeric@ik.me>
2024-05-11 16:51:54 +0200

    first commit

---
+++ site_lueur_du_mont/reseaux.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title> Page Reseaux</title>
+
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="css/flaticon.css">
+
+        <link rel="stylesheet" href="css/animate.css">
+
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+		<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<link rel="stylesheet" href="css/bootsnav.css" >	
+
+        <link rel="stylesheet" href="css/tyle.css">
+
+        <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+    <h1><center> Nos réseaux </center></h1>
+    <hr>
+    <br>
+    <section id="instagram" class="instagram">
+			<div class="portfolio-details">
+				<div class="section-heading text-center">
+					<h2>Instagram</h2>
+
+        <br>
+        <b>Soyez à l'actualité !</b>
+        <br><br>
+        <rssapp-wall id="c3Q8MVm86b7jOuEk"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
+        <h2> Facebook </h2>
+        <br><br><br><br><br><br><br>
+        <h2> X </h2>
+
+</body>
+</html>
